a99f6313c906ea4a9ee22f4ab016989a82bb3a38
LucasCelestino <lucasrashido@gmail.com>
2022-02-18 20:35:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-ibw8hy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ibw8hy)
---
+++ index.ts
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+if (true) {
+}
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
